0188f269b74d43bd0e4d22426b00409c4e79fb4c
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-12 17:56:22 +0900

    theming

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	let topAppBar: TopAppBar;
 </script>
 
-<TopAppBar bind:this={topAppBar} variant="fixed" color="primary">
+<TopAppBar bind:this={topAppBar} variant="fixed" color="secondary" class="mdc-elevation--z1">
 	<Row>
 		<Section>
 			<img class="logo" src="/icon.png" alt="" />
