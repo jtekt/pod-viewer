eb7568720d71b1dace58c8d2eb586eaff36a8dbf
Maxime Moreillon <m.moreillon@gmail.com>
2024-08-22 08:12:08 +0900

    improved state check logic

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -4,10 +4,11 @@ export const formaPodtState = (pod: V1Pod) => {
 	let state = pod.status?.phase;
 	const containerStatuses = pod.status?.containerStatuses;
 
-	if (containerStatuses) {
-		const waitingContainer = containerStatuses.find((c) => c.state?.waiting);
-		if (waitingContainer) state = waitingContainer.state?.waiting?.reason;
-	}
+	if (!containerStatuses) return state;
+
+	// If at least one container is waiting, then state becomes the reason
+	const waitingContainer = containerStatuses.find((c) => c.state?.waiting);
+	if (waitingContainer) state = waitingContainer.state?.waiting?.reason;
 
 	return state;
 };
